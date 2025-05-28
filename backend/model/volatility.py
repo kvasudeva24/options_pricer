5cc2322770bcc4eafed81ed60e3b4098f9ee7fa1
kvasudeva24 <vasudevakartik@gmail.com>
2025-05-28 12:33:43 -0500

    added documentation

--- backend/model/volatility.py
+++ backend/model/volatility.py
@@ -18,17 +18,16 @@ def historical_volatility(ticker, period):
 
     Returns: Historical volatility as a float
     """
-    data = yf.Ticker(ticker)
-    hist = data.history(period=f"{period}d")
-    closing_prices = hist["Close"]
-    closing_prices = np.array(closing_prices)
-    returns = np.zeros(closing_prices.shape[0])
+    data = yf.Ticker(ticker) #fetch data
+    hist = data.history(period=f"{period}d") #get historical data 
+    closing_prices = hist["Close"] 
+    closing_prices = np.array(closing_prices) #get values as numpy array
+    returns = np.zeros(closing_prices.shape[0]) #initialize returns
     for i in range(1, len(closing_prices)):
         returns[i] = ((closing_prices[i] - closing_prices[i-1])/closing_prices[i-1])
-    returns = returns[1:]
+    returns = returns[1:] #remove first element 
     volatility = np.std(returns) #Std dev of returns
     return volatility * np.sqrt(252) #Annualize the volatility
 
 
-print(historical_volatility("AAPL", 252))
 
