5fef7706858e8b6cb8de30c66ffe63b1320129f7
kvasudeva24 <vasudevakartik@gmail.com>
2025-05-31 16:30:15 -0500

    set up skeleton for call

--- backend/model/pricer.py
+++ backend/model/pricer.py
@@ -5,3 +5,18 @@ from volatility import parkinsons_volatility
 from volatility import garman_klass_volatility
 from volatility import rogers_satchell_volatility
 from volatility import yang_zhang_volatility
+from scipy.stats import Norm as N
+
+
+def call_option(ticker, period_vol, period_opt):
+    """
+    Calculate the BSM price of a stock in a given period (time remaining and time for volatility).
+    
+
+    Parameters: 
+    ticker: Stock ticker
+    period_vol: Period in trading days to calculate volatility
+    period: time remaining on the optoop
+
+    Returns: BSM call-option price as a float
+    """
