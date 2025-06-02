97d969341981e5d01bb78221ca3de4765a5b6804
kvasudeva24 <vasudevakartik@gmail.com>
2025-06-01 21:01:26 -0500

    figured out API and now messing arounf with it

---
+++ backend/model/heatmap.py
@@ -0,0 +1,32 @@
+import yfinance as yf
+import numpy as np
+import pandas as pd
+from pricer import (call_option, put_option)
+
+ticker = "TSLA"
+data = yf.Ticker(ticker)
+hist = data.history(period="1d")
+all_dates = data.options #dynamically get the option dates 
+print(all_dates) 
+
+
+opt_chain = data.option_chain("2025-06-06")
+call_opt = opt_chain.calls
+# put_opt = opt_chain.puts
+
+curr_price = hist["Close"].iloc[-1]
+lower_bound = 0.8 * curr_price
+upper_bound = 1.2 * curr_price
+
+filtered_rows = []
+
+for i, row in call_opt.iterrows():
+    if lower_bound < row['strike'] < upper_bound:
+        filtered_rows.append(row)
+
+filtered = pd.DataFrame(filtered_rows)
+print(filtered) #getting all the prices within 20% of an option 
+
+
+
+print(type(call_opt)) #it is a pandas dataframe
