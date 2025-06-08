5056526bda6e4d5128513af8bbd3a7574e90d335
kvasudeva24 <vasudevakartik@gmail.com>
2025-06-08 14:38:57 -0500

    backend good for running

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from backend.pricer import (call_option, put_option)
 from backend.heatmap import (call_option_heatmap, put_option_heatmap)
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('api/call-price', methods=['POST'])
+@app.route('/api/call-price', methods=['POST'])
 def get_call_price():
     data = request.get_json()
     try:
@@ -20,7 +22,7 @@ def get_call_price():
         return jsonify({'error' : str(e)}), 500
     
 
-@app.route('api/put-price', methods=['POST'])
+@app.route('/api/put-price', methods=['POST'])
 def get_put_price():
     data = request.get_json()
     try:
@@ -34,3 +36,36 @@ def get_put_price():
         return jsonify({'price' : result})
     except Exception as e:
         return jsonify({'error' : str(e)}), 500
+    
+
+@app.route('/api/call-heatmap', methods=['POST'])
+def get_call_heatmap():
+    data = request.get_json()
+    try:
+        result = call_option_heatmap(
+            ticker=data['ticker'],
+            option_vol=data['option_vol'],
+            period_vol=data['period_vol'],
+        )
+        return jsonify({'heatmap' : result})
+    except Exception as e:
+        return jsonify({'error' : str(e)}), 500
+    
+
+@app.route('/api/put-heatmap', methods=['POST'])
+def get_put_heatmap():
+    data = request.get_json()
+    try:
+        result = put_option_heatmap(
+            ticker=data['ticker'],
+            option_vol=data['option_vol'],
+            period_vol=data['period_vol']
+        )
+        return jsonify({'heatmap' : result})
+    except Exception as e:
+        return jsonify({'error' : str(e)}), 500
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
