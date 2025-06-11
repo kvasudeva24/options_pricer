8a836ca879cc4dacdf75aeeea2e35f86361235a0
kvasudeva24 <vasudevakartik@gmail.com>
2025-06-10 23:55:02 -0500

    added all buttons for put/call inputs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   const [optionType, setOptionType] = useState('select'); 
+  const [volatilityType, setVolatilityType] = useState('select');
 
   return (
     <div className="App">
@@ -28,6 +29,21 @@ function App() {
           <option value="put">Put Option</option>
         </select>
         <input className="tickerInput" type = "text" placeholder='Ticker Symbol'/>
+        <input className="strikeInput" type = "text" placeholder='Strike Price'/>
+        <input className="daysInput" type = "text" placeholder='Trading Days to Expiry'/>
+        <select
+          className="volatilityInput"
+          value={volatilityType}
+          onChange={(e) => setVolatilityType(e.target.value)}
+          >
+            <option value="select">Select Volatility Type</option>
+            <option value="historical">Historical Log</option>
+            <option value="parkinsons">Parkinson's</option>
+            <option value="garman-klass">Garman-Klass</option>
+            <option value="rogers-satchel">Rogers-Satchel</option>
+            <option value="yang-zhang">Yang-Zhang</option>
+          </select>
+          <input className="volatilityDaysInput" type = "text" placeholder="Period of Vol. (days)"/>
       </div>
     </div>
   );
