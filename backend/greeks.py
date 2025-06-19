20c602ffabb5141d1fed71b5dbaa2bd2b3cec592
kvasudeva24 <vasudevakartik@gmail.com>
2025-06-19 14:35:16 -0500

    added all 4 greeks

--- backend/greeks.py
+++ backend/greeks.py
@@ -19,6 +19,8 @@ def get_greeks(opt_type, ticker, strike_price ,option_vol, period_vol, period_op
     output = {
         "Delta": get_delta(opt_type, ticker, strike_price ,option_vol, period_vol, period_opt, output, stock_price),
         "Gamma": get_gamma(opt_type, ticker, strike_price ,option_vol, period_vol, period_opt, output, stock_price),
+        "Theta": get_theta(opt_type, ticker, strike_price ,option_vol, period_vol, period_opt, output, stock_price),
+        "Rho": get_rho(opt_type, ticker, strike_price ,option_vol, period_vol, period_opt, output, stock_price)
     }
     return output
 
@@ -82,3 +84,32 @@ def get_theta(opt_type, ticker, strike_price ,option_vol, period_vol, period_opt
         new_option_price = strike_price * np.exp(-risk_free_rate * new_adjusted_T) * norm.cdf(-d2) - stock_price * norm.cdf(-d1)
 
     return (new_option_price - output)/(-1)
+
+def get_rho(opt_type, ticker, strike_price ,option_vol, period_vol, period_opt, output, stock_price):
+    if option_vol == 1:
+        vol = historical_log_volatility(ticker, period_vol)
+    elif option_vol == 2:
+        vol = parkinsons_volatility(ticker, period_vol)
+    elif option_vol == 3:
+        vol = garman_klass_volatility(ticker, period_vol)
+    elif option_vol == 4:
+        vol = rogers_satchell_volatility(ticker, period_vol)
+    else:
+        vol = yang_zhang_volatility(ticker, period_vol)
+
+    # Convert time to years
+    adjusted_T = period_opt / 252.0
+
+    #new rfir
+    new_risk_free_rate = risk_free_rate + 0.01
+
+    # Black-Scholes-Merton calculation
+    d1 = (np.log(stock_price / strike_price) + (new_risk_free_rate + 0.5 * vol ** 2) * adjusted_T) / (vol * np.sqrt(adjusted_T))
+    d2 = d1 - vol * np.sqrt(adjusted_T)
+
+    if(opt_type == 0):
+        new_option_price = stock_price * norm.cdf(d1) - strike_price * np.exp(-new_risk_free_rate * adjusted_T) * norm.cdf(d2)
+    else:
+        new_option_price = strike_price * np.exp(-new_risk_free_rate * adjusted_T) * norm.cdf(-d2) - stock_price * norm.cdf(-d1)
+
+    return (new_option_price - output)/(0.01)
